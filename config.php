8a72de56f3d6e276d4f4c60800a25d9cb277cd1f
lfpo2005 <47955596+lfpo2005@users.noreply.github.com>
2020-03-25 20:35:30 -0300

    phpMailer

--- config.php
+++ config.php
@@ -1,4 +1,14 @@
 <?php
+
+$autoload = function($class){
+    if($class == 'Email'){
+        include('classes/vendor/autoload.php');
+    }
+    include('classes/'.$class.'.php');
+};
+
+spl_autoload_register($autoload);
+
 define('INCLUDE_PATH','http://localhost/ProjetoPhp/');
 
 ?>
